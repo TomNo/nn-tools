b4a464ce1f791ae7fa3fe2d9d8d8a8ee975c33f3
Tomáš Nováčik <tnovacik@gmail.com>
2015-11-09 11:20:30 +0100

    Refactor && normalization

--- nn_tools/data_convertor.py
+++ nn_tools/data_convertor.py
@@ -7,91 +7,138 @@ import h5py
 import kaldi_io
 import sys
 import numpy as np
-from Scientific.IO.NetCDF import NetCDFFile
 
-def get_kaldi_data(options):
-    """Gets kaldi tags, features and alignments."""
-    tags = []
-    mats = []
-    sizes = []
-    l_dict = {}
-    if not options.forward_pass:
-        for tag, l in kaldi_io.read_ali_ark(options.labels):
-            l_dict[tag] = l
-    for tag, mat in kaldi_io.read_mat_scp(options.features):
-        tags.append(tag)
-        mats.append(mat)
-        sizes.append(len(mat))
+# TODO how to pass std/mean to test data
 
-        if not options.forward_pass:
-            if len(mat) != len(l_dict[tag]):
-                raise ValueError("Labels lengths does not match data length.")
-    return tags, mats, sizes, l_dict
+class Convertor(object):
+    def __init__(self, options):
+        self.options = options
 
+    def convert(self):
+        if self.options.action.startswith("kaldi"):
+            self.kaldi_convert()
+        elif self.options.action == 'hdf-to-kaldi':
+            self.hdf2k()
+        else:
+            raise Exception("Unsupported action.")
 
-def k2nc(options):
-    """Convert kaldi data to netcdf format (currennt)."""
-    o_file =  NetCDFFile(options.output_file, 'w')
-    tags, mats, sizes, l_dict = get_kaldi_data(options)
-    seq_lengths = [len(mat) for mat in mats]
-    inputs = []
-    for mat in mats:
-        for line in mat:
-            inputs.append(line)
-    num_labels = 0
-    if not options.forward_pass:
-        num_labels = max(max(l_dict.values(), key=max)) + 1
+    def kaldi_convert(self):
+        if self.options.deltas_order > 0:
+            self.get_kaldi_data()
+            self.add_deltas()
+        if self.options.normalize:
+            self.normalize()
+        if self.options.action.endswith("nc"):
+            self.k2nc()
+        elif self.options.action.endswith("hdf"):
+            self.k2hdf()
 
-    def c_var(vname, data, vtype, dims):
-        nc_var = o_file.createVariable (vname,vtype,dims)
-        nc_var.assignValue(data)
+    def add_deltas(self):
+        pass
 
-    labels = []
-    if options.forward_pass:
-        for i in seq_lengths:
-            labels +=[0] * i
-    else:
-        for i in tags:
-            labels += l_dict[i].tolist()
-    o_file.createDimension('numSeqs', len(seq_lengths))
-    o_file.createDimension('numTimesteps', len(inputs))
-    o_file.createDimension('inputPattSize', len(inputs[0]))
-    o_file.createDimension('numLabels', num_labels)
+    def normalize(self):
+        m = 0
+        std = 0
+        if not self.options.mean:
+            for mat in self.mats:
+                m+=mat.mean()
+        else:
+            m=self.options.mean
 
-    c_var('inputs', inputs, 'f', ('numTimesteps', 'inputPattSize'))
-    c_var('seqLengths', seq_lengths, 'i', ('numSeqs',))
-    c_var('targetClasses', labels, 'i',('numTimesteps',))
+        if not self.options.std:
+            for mat in self.mats:
+                std+=mat.std()
+        else:
+            std = self.options.std
 
-    m_s_length = len(max(tags, key=len))
-    n_tags = []
-    for tag in tags:
-        n_tags.append(list(tag) +['\0']*(m_s_length - len(tag)))
-    o_file.createDimension('maxSeqTagLength', m_s_length)
-    c_var('seqTags', np.array(n_tags), 'c', ('numSeqs', 'maxSeqTagLength'))
-    o_file.close()
+        m /= float(len(self.mats))
+        std /= float(len(self.mats))
 
+        for i in xrange(len(self.mats)):
+            self.mats[i] -= m
+            self.mats[i] /= std
 
-def k2hdf(options):
-    """Converts kaldi data to hdf5"""
-    with h5py.File(options.output_file, "w") as o_file:
-        tags, mats, sizes, l_dict = get_kaldi_data(options)
-        # must be done like this because torch-hdf5 does not support strings nor attributes:'(
-        for index, tag in enumerate(tags):
-            g = o_file.create_group(tag)
-            if not options.forward_pass:
-                g.create_dataset("labels", data=l_dict[tag])
-            g.create_dataset("data", data=mats[index].flatten())
-            g.create_dataset("cols", data=[len(mats[index][0])])
-            g.create_dataset("rows", data=[len(mats[index])])
+    def get_kaldi_data(self):
+        """Gets kaldi tags, features and alignments."""
+        tags = []
+        mats = []
+        sizes = []
+        l_dict = {}
+        if not self.options.forward_pass:
+            for tag, l in kaldi_io.read_ali_ark(self.options.labels):
+                l_dict[tag] = l
+        for tag, mat in kaldi_io.read_mat_scp(self.options.features):
+            tags.append(tag)
+            mats.append(mat)
+            sizes.append(len(mat))
 
+            if not self.options.forward_pass:
+                if len(mat) != len(l_dict[tag]):
+                    raise ValueError("Labels lengths does not match data length.")
+        self.tags = tags
+        self.mats = mats
+        self.sizes = sizes
+        self.l_dict = l_dict
 
-def hdf2k(options):
-    """Converts hdf5 format to kaldi matrix."""
-    with h5py.File(options.net_output) as i_file:
-        with open(options.output_file, "w") as o_file:
-            for key, item in i_file.items():
-                mat, cols, rows = item["data"], item["cols"][0], item["rows"][0]
-                kaldi_io.write_mat(o_file, mat.value.reshape(rows, cols), key)
+    def k2nc(self):
+        """Convert kaldi data to netcdf format (currennt)."""
+        o_file =  NetCDFFile(self.options.output_file, 'w')
+        seq_lengths = [len(mat) for mat in self.mats]
+        inputs = []
+        for mat in self.mats:
+            for line in mat:
+                inputs.append(line)
+        num_labels = 0
+        if not self.options.forward_pass:
+            num_labels = max(max(self.l_dict.values(), key=max)) + 1
+
+        def c_var(vname, data, vtype, dims):
+            nc_var = o_file.createVariable (vname,vtype,dims)
+            nc_var.assignValue(data)
+
+        labels = []
+        if self.options.forward_pass:
+            for i in seq_lengths:
+                labels +=[0] * i
+        else:
+            for i in self.tags:
+                labels += self.l_dict[i].tolist()
+        o_file.createDimension('numSeqs', len(seq_lengths))
+        o_file.createDimension('numTimesteps', len(inputs))
+        o_file.createDimension('inputPattSize', len(inputs[0]))
+        o_file.createDimension('numLabels', num_labels)
+
+        c_var('inputs', inputs, 'f', ('numTimesteps', 'inputPattSize'))
+        c_var('seqLengths', seq_lengths, 'i', ('numSeqs',))
+        c_var('targetClasses', labels, 'i',('numTimesteps',))
+
+        m_s_length = len(max(self.tags, key=len))
+        n_tags = []
+        for tag in self.tags:
+            n_tags.append(list(tag) + ['\0']*(m_s_length - len(tag)))
+        o_file.createDimension('maxSeqTagLength', m_s_length)
+        c_var('seqTags', np.array(n_tags), 'c', ('numSeqs', 'maxSeqTagLength'))
+        o_file.close()
+
+    def k2hdf(self):
+        """Converts kaldi data to hdf5"""
+        with h5py.File(self.options.output_file, "w") as o_file:
+            # must be done like this because torch-hdf5 does not support strings nor attributes:'(
+            for index, tag in enumerate(self.tags):
+                g = o_file.create_group(tag)
+                if not self.options.forward_pass:
+                    g.create_dataset("labels", data=self.l_dict[tag])
+                g.create_dataset("data", data=self.mats[index].flatten())
+                g.create_dataset("cols", data=[len(self.mats[index][0])])
+                g.create_dataset("rows", data=[len(self.mats[index])])
+
+    def hdf2k(self):
+        """Converts hdf5 format to kaldi matrix."""
+        with h5py.File(self.options.net_output) as i_file:
+            with open(self.options.output_file, "w") as o_file:
+                for key, item in i_file.items():
+                    mat, cols, rows = item["data"], item["cols"][0], item["rows"][0]
+                    kaldi_io.write_mat(o_file, mat.value.reshape(rows, cols), key)
 
 usage = 'usage: %prog [options]'
 parser = argparse.ArgumentParser(usage)
@@ -101,8 +148,15 @@ parser.add_argument('--forward-pass', dest='forward_pass', action='store_true', 
 parser.add_argument('-l', '--labels', dest='labels', action='store', type=str, help='labels of the data')
 parser.add_argument('--net-output', dest='net_output', action='store', type=str, help='output of neural network in hdf file format')
 parser.add_argument('-o', '--output-file', dest='output_file', action='store', type=str,  help='output file name', required=True)
+parser.add_argument('--normalize', dest='normalize', action='store', type=bool,  help='normalize input, only usable for kaldi conversion', default=True)
+parser.add_argument('--deltas-order', dest='deltas_order', action='store', type=int, help='add deltas, only usable for from kaldi conversion', default=0)
+parser.add_argument('--mean', dest="mean", action="store", type=float, help="mean that will be substracted from the input data", default=None)
+parser.add_argument('--std', dest="std", action="store", type=float, help="standard deviation that will divide input input data", default=None)
 
 options = parser.parse_args()
+
+if "nc" in options.action:
+    from Scientific.IO.NetCDF import NetCDFFile
 
 if options.action.startswith("kaldi") and (not options.features or not options.labels):
     if options.forward_pass and not options.features:
@@ -112,11 +166,22 @@ if options.action.startswith("kaldi") and (not options.features or not options.l
         print("Error:Missing features or label files.")
         sys.exit(1)
 
+if not options.action.startswith("kaldi"):
+    if options.normalize != False:
+        print("Error:Normalization is done only when converting data from kaldi.")
+        sys.exit(1)
+    if options.deltas_order != 0:
+        print("Error:Deltas order must be 0 (Only used for from kaldi conversion)")
+        sys.exit(1)
 if not options.action.startswith("kaldi") and not options.net_output:
     print("Error:Missing net output filename.")
     sys.exit(1)
 
-actions = dict([('kaldi-to-hdf', k2hdf) ,('hdf-to-kaldi', hdf2k),
-                ('kaldi-to-nc', k2nc)])
-actions[options.action](options)
+if (options.mean or options.std) and not options.normalize:
+    print("Mean and std specified although normalization was not selected.")
+    sys.exit(1)
+
+convertor = Convertor(options)
+convertor.convert()
+
 # eof
